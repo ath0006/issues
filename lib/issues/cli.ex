dcc7a2f43ff4971573fdb1b63408df30f5f250a5
unknown <ahuggins0006@gmail.com>
2017-11-25 19:57:31 -0600

    code to complete the transformation from raw data to tabular format

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,6 +1,7 @@
 defmodule Issues.CLI do
   
   @default_count 4
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
 
   @moduledoc """
   Handle the command line parsing and the dispatch to
@@ -9,7 +10,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc  """
@@ -27,7 +30,7 @@ defmodule Issues.CLI do
         -> :help
 
       { _, [user, project, count ], _ }
-        -> { user, project, count }
+        -> { user, project, String.to_integer(count) }
 
       { _, [user, project ], _ }
         -> {user, project, @default_count}
@@ -35,4 +38,34 @@ defmodule Issues.CLI do
       _ -> :help
     end
   end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 -> Map.get(i1, "created_at") <= Map.get(i2, "created_at") end
+  end
+
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
 end
